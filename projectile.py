2fd9f0ade4087dc9746abcdae3514d0714170ec6
Mohamed Moslemani <moslemanomohamed@gmail.com>
2024-10-25 22:53:01 +0300

    pygame initialization + initial drawer function

--- projectile.py
+++ projectile.py
@@ -2,3 +2,19 @@ import pygame
 import math
 import sys
 
+pygame.init()
+
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption('Projectile Motion')
+font = pygame.font.SysFont(None, 24)
+
+clock = pygame.time.clock()
+fps = 60
+
+gravity = 9.8
+projectiles = []
+
+def draw_text(text, color, x, y):
+    img = font.render(text, True, color)
+    screen.blit(img, (x, y))
+    
